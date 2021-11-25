cadd6f99a89bd85157de56c119057a964bdfdb8c
ashu5127 <93268981+ashu5127@users.noreply.github.com>
2021-11-25 14:47:32 +0530

    Add files via upload

---
+++ Calculate the volume of a cube.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+float cube=5;
+float volume;
+
+cout<<"Calculate the volume of a cube :"<<endl;
+cout<<"---------------------------------------"<<endl;
+cout<<"Input the side of a cube :"<<cube<<endl; //5
+volume = cube*cube*cube;
+cout<<"The volume of a cube is :"<<volume<<endl;//125
+
+return 0;
+}
